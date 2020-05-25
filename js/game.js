aba496c78761586b16ae39fafd27ed4d53a880f3
Irvino Djuana <irvino.djuana@gmail.com>
2020-05-25 16:08:00 -0700

    Fix - Refactored game.js code, tweaked AI settings

--- js/game.js
+++ js/game.js
@@ -1,5 +1,6 @@
 /*
- * Model class that handles the state of the game
+ * Class that handles the state of the game
+ * and calls methods to update the UI
  */
 class Game {
     constructor(difficulty, playerBoard) {
@@ -8,6 +9,77 @@ class Game {
         this.enemyBoard = null;
         this.playerSelect = null;
         this.progressState = null;
+        this.NUM_SHIP_CELL = Object.values(SIZES).reduce((a, b) => a + b, 0);
+    }
+
+    /* Update game state with player selected shot, return whether shot is valid */
+    processPlayerShot() {
+        // parse selected cell and update state
+        let i = this.playerSelect.charCodeAt(0) - 'A'.charCodeAt(0);
+        let j = parseInt(this.playerSelect[1]);
+        let cell = this.enemyBoard[i][j]
+        if (cell == HIT || cell == MISS) {
+            console.log("WARNING: Player has chosen a previously selected cell. Choose another cell.")
+            return false;
+        } else {
+            this.enemyBoard[i][j] = (cell == EMPTY) ? MISS : HIT;
+        }
+        return true;
+    }
+
+    /* Update game state  with enemy selected shot */
+    processEnemyShot() {
+        // request AI selection and inform if hit or miss
+        let [i, j] = this.enemyAI.selectTarget();
+        let cell = this.playerBoard[i][j];
+
+        if (cell == HIT || cell == MISS) {
+            console.log("WARNING: AI has chosen a previously selected cell;")
+        }
+        cell = (cell == EMPTY || cell == MISS) ? MISS : HIT;
+        this.enemyAI.updateHit(i, j, cell == HIT, this.playerBoard[i][j]);
+        this.playerBoard[i][j] = cell;
+    }
+
+    /* Update the enemy-side UI (for the player) and return if player won */
+    updateEnemySide(newEnemyState) {
+        // display to user if enemy ship sunk
+        for (let ship of SHIPS) {
+            if (this.enemyState[ship].length > 0 && newEnemyState[ship].length == 0) {
+                displaySink("ai", ship)
+            }
+        }
+        // render enemy board to screen and check player win-state
+        this.enemyState = newEnemyState;
+        render(this.enemyState, false);
+
+        // check if player has won
+        if (this.enemyState[HIT].length === this.NUM_SHIP_CELL) {
+            displayWinner("player", "ai");
+            return true;
+        }
+        return false;
+    }
+
+    /* Update the player-side UI (for the AI actions) and return if enemy won */
+    updatePlayerSide(newPlayerState) {
+        // Inform AI and display if ship sunk
+        for (let ship of SHIPS) {
+            if (this.playerState[ship].length > 0 && newPlayerState[ship].length == 0) {
+                this.enemyAI.updateSink(ship);
+                displaySink("player", ship)
+            }
+        }
+        // render player board to screen and check enemy win-state
+        this.playerState = newPlayerState;
+        render(this.playerState);
+
+        // check if AI has won
+        if (this.playerState[HIT].length === this.NUM_SHIP_CELL) {
+            displayWinner("ai", "player");
+            return true;
+        }
+        return false;
     }
 
     /* Starts the game */
@@ -38,85 +110,32 @@ class Game {
         render(this.playerState);
         render(this.enemyState, false);
 
-        // TODO: Setup phase - select ships
-
         // Play phase
-        const NUM_SHIP_CELL = Object.values(SIZES).reduce((a, b) => a + b, 0);
-        let i, j, cell;
-        let num_turns = 0;
-
         while (true) {
             // loop/wait until player has selected a cell
             this.playerSelect = null;
             while (this.playerSelect === null) {
                 await sleep(100);
             }
-
-            // parse selected cell and update state
-            i = this.playerSelect.charCodeAt(0) - 'A'.charCodeAt(0);
-            j = parseInt(this.playerSelect[1]);
-            cell = this.enemyBoard[i][j]
-            if (cell == HIT || cell == MISS) {
-                console.log("WARNING: Player has chosen a previously selected cell. Choose another cell.")
-                continue; // already fired - no penalty
-            } else {
-                this.enemyBoard[i][j] = (cell == EMPTY) ? MISS : HIT;
-            }
-
-            // request AI selection and inform if hit or miss
-            [i, j] = this.enemyAI.selectTarget();
-            cell = this.playerBoard[i][j];
-
-            if (cell == HIT || cell == MISS) {
-                console.log("WARNING: AI has chosen a previously selected cell;")
-            }
-            cell = (cell == EMPTY || cell == MISS) ? MISS : HIT;
-            this.enemyAI.updateHit(i, j, cell == HIT, this.playerBoard[i][j]);
-            this.playerBoard[i][j] = cell;
+            // process shots chosen - if player selects invalid (prev. selected) cell, retry turn
+            let valid = this.processPlayerShot();
+            if (!valid) continue;
+            this.processEnemyShot();
             
             // fetch board state maps
             const newEnemyState = getBoardState("enemyboard", this.enemyBoard);
             const newPlayerState = getBoardState("playerboard", this.playerBoard);
 
-
-            // display to user if enemy ship sunk
-            for (let ship of SHIPS) {
-                if (this.enemyState[ship].length > 0 && newEnemyState[ship].length == 0) {
-                    displaySink("ai", ship)
-                }
-            }
-            // render enemy board to screen and check player win-state
-            this.enemyState = newEnemyState;
-            render(this.enemyState, false);
-
-            // check if player has won
-            if (this.enemyState[HIT].length === NUM_SHIP_CELL) {
-                displayWinner("player", "ai");
-                break;
-            }
+            // update the UI, end game if winner found
+            let playerWon = this.updateEnemySide(newEnemyState)
+            if (playerWon) break;
 
             await sleep(300); // delay to let enemy "choose"
-            
-            // Inform AI and display if ship sunk
-            for (let ship of SHIPS) {
-                if (this.playerState[ship].length > 0 && newPlayerState[ship].length == 0) {
-                    this.enemyAI.updateSink(ship);
-                    displaySink("player", ship)
-                }
-            }
-            // render player board to screen and check enemy win-state
-            this.playerState = newPlayerState;
-            render(this.playerState);
 
-            // check if AI has won
-            if (this.playerState[HIT].length === NUM_SHIP_CELL) {
-                displayWinner("ai", "player");
-                console.log(`Number of turns taken: ${num_turns+1}`);
-                break;
-            }
-
-            num_turns += 1;
+            let enemyWon = this.updatePlayerSide(newPlayerState);
+            if (enemyWon) break;
         }
+
         this.endGame();
     }
 
